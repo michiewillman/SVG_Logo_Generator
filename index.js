cb2c2b29f5d7f110f048e98b8b8c6976ea071fb9
Michie <contact@michiewillman.com>
2023-07-12 23:31:42 -0700

    Modify init .thens

--- index.js
+++ index.js
@@ -3,25 +3,25 @@ const fs = require("fs");
 const {Circle, Square, Triangle} = require('./lib/shapes');
 const questions = require('./lib/questions');
 
-// new Shapes.Circle
-// let shape
-// switch(answers.shape){
-//   case "circle":
-//      shape=new Circle()
-//     shape.setColor(answers.color)
+new Shapes.Circle
+let shape
+switch(answers.shape){
+  case "circle":
+     shape=new Circle()
+    shape.setColor(answers.color)
 
-// }
-// generateSVG(shape)
+}
+generateSVG(shape)
 
 
 // Generates svg content
 function generateSVG(data) {
-  const svgContent = (
+  const svgContent = 
     `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
       ${data.xml}
       <text x="150" y="125" font-size="60" text-anchor="middle" fill="${data.textColor}">${data.text}</text>
-    </svg>`
-  );
+    </svg>`;
+  return svgContent;
 }
 
 // Writes svg content to file
@@ -35,18 +35,19 @@ function writeToFile(fileName, content) {
 function init() {
   inquirer
   .prompt(questions)
-  .then((response) => {
-    console.log(response);
-    return shape.render();
-  .then((response) => {
-    console.log(response);
-    return generateSVG(response)
-  .then((response) => {
+  .then((answers) => {
+    console.log(answers);
+    const newShape = answers.shape;
+    return newShape.render()
+  })
+  .then((shape) => {
+    return generateSVG(shape)
+  })
+  .then((svg) => {
     // TODO: Add dynamically named svg? (ex: MW-green-circle-logo.svg)
-    writeToFile("my_logo.svg", response)
+    writeToFile("my_logo.svg", svg)
   })
   .catch((error) => {
     console.log(error)
-    });
   });
 }
