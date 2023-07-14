1198d04a1c0246b54d573cffb54d1dc5b41d4b38
Michie <contact@michiewillman.com>
2023-07-14 12:46:49 -0700

    Move functions to generateSvg.js

--- Utils/generateSvg.js
+++ Utils/generateSvg.js
@@ -1,3 +1,31 @@
+const {Circle, Square, Triangle} = require('./lib/shapes');
 
+// Gets shape from answers --> calls on classes
+function getShape(answers) {
+  let shape;
+  switch (answers.shape) {
+    case "circle":
+      shape = new Circle();
+      break;
+    case "square":
+      shape = new Square();
+      break;
+    case "triangle":
+      shape = new Triangle();
+      break;
+  }
+  shape.setColor(answers.color);
+  shape.setText(answers.text.toUpperCase());
+  shape.setTextColor(answers.textColor);
+  return shape;
+}
 
-
+// Generates svg content --> render() pulls in shape xml string with shape fill color
+function generateSVG(data) {
+  const svgContent = 
+    `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      ${data.render()}
+      <text x="150" y="125" font-size="60" text-anchor="middle" fill="${data.textColor}">${data.text}</text>
+    </svg>`;
+  return svgContent;
+}
