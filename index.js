532542f437c120745e90e3cbb252897fd3003ae8
Michie <contact@michiewillman.com>
2023-07-14 13:37:04 -0700

    Add validation for text function

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const fs = require("fs");
 const questions = require('./lib/questions');
 const { getShape, generateSVG } = require('./Utils/generateSvg');
 
+function validateText(answers) {
+  if (answers.text.length > 3 || answers.text.length < 0) {
+    console.log("Text must be between 1 and 3 characters");
+    return;
+  } else {
+    return answers;
+  }
+}
+
 // Starts application on page load/ready
 function init() {
   inquirer
   .prompt(questions)
+  .then((answers) => {
+    return validateText(answers);
+  })
   .then((answers) => {
     return getShape(answers);
   })
